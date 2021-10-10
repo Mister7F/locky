efca9d2e4cd1f1d31eb1097256e261790b7d3210
Mister7F <mister7f@gmail.com>
2021-10-10 15:27:28 +0200

    [FIX] img: fix the "noimage.svg" to be set in a loop
    Bug
    ===
    If the service worker is disabled and if the user has no internet
    connection, the image "noimage.svg" will be set indefinitely.

--- svelte/src/application/Img.svelte
+++ svelte/src/application/Img.svelte
@@ -4,8 +4,11 @@
     let className = '';
     export { className as class };
 
+    let noimageSet = false;
+
     function onError(event) {
-        if (event.target.src !== 'img/noimage.svg') {
+        if (event.target.src !== 'img/noimage.svg' && !noimageSet) {
+            noimageSet = true;
             event.target.src = 'img/noimage.svg';
         }
     }
